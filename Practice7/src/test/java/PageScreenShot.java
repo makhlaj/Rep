81ef6caacc61c7e72a37ac2c730b9fb49f606438
Makhlaj <Makhlaj>
2016-11-27 08:25:55 +0200

    last V Practice7

---
+++ Practice7/src/test/java/PageScreenShot.java
@@ -0,0 +1,30 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+
+
+import java.io.File;
+import java.io.IOException;
+
+public class PageScreenShot {
+
+    private WebDriver driver;
+    private WebElement screenFileName;
+    private File screenFile;
+    private String path;
+
+    public PageScreenShot(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void getPageScreenShot() {
+
+        screenFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        screenFileName = driver.findElement(By.tagName("h1"));
+        path = "./target/screenshots/" + screenFileName.getText() + ".png";
+        try {
+            FileUtils.copyFile(screenFile, new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
